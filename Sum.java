eba3d301d35aef10d03e29b610099233f2cc891a
Manish Shendge <manishshendge2009@gmail.com>
2023-02-19 14:05:52 +0530

    one more commit

--- Sum.java
+++ Sum.java
@@ -3,11 +3,10 @@ package gernal_practice;
 public class Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int a=5;
 		int b=6;
 		System.out.println(a+b);
-
+// commit check
 	}
 
 }
