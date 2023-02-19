7d49bc62a336b160cfe7c98e0420724ad263ea3b
Manish Shendge <manishshendge2009@gmail.com>
2023-02-19 13:15:11 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package gernal_practice;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		System.out.println(a+b);
+
+	}
+
+}
